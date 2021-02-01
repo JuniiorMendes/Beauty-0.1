92eb994671d523b618d59271a60780cfcdaa639b
JuniiorMendes <juniiormendesofc@gmail.com>
2021-02-01 17:48:03 -0300

    atualização da galeria

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function galeria() {
+  let time = 5000,
+    ImgIndex = 0,
+    imagens = document.querySelectorAll('.imgs-galeria img')
+  max = imagens.length
+  imagens[ImgIndex].classList.add('selecionado')
+
+  function proximaImg() {
+    imagens[ImgIndex].classList.remove('selecionado')
+    ImgIndex++
+
+    if (ImgIndex >= max) {
+      ImgIndex = 0
+    }
+
+    imagens[ImgIndex].classList.add('selecionado')
+  }
+
+  function inicio() {
+    setInterval(() => {
+      proximaImg()
+    }, time)
+  }
+
+  window.addEventListener('load', inicio)
+
+}
+
+galeria()
